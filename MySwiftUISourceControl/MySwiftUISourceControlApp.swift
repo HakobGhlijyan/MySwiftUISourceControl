e4dcf33beb043550237a2116e1a060552923075e
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-03 12:59:53 +0400

    Initial Commit

---
+++ MySwiftUISourceControl/MySwiftUISourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUISourceControlApp.swift
+//  MySwiftUISourceControl
+//
+//  Created by Hakob Ghlijyan on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUISourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
