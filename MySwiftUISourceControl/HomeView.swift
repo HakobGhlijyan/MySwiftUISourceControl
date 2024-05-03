fd6d4318ed67f2da84db36ade05b4e36bfbef0bc
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-03 14:34:05 +0400

    Add HomeView

---
+++ MySwiftUISourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MySwiftUISourceControl
+//
+//  Created by Hakob Ghlijyan on 03.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
