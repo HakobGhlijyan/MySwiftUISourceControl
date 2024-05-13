ff4e02aa45a73189933392c8c16b52da3bc75db0
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-13 18:09:16 +0400

    [Feature] Fix world to Hakob

--- MySwiftUISourceControl/HomeView.swift
+++ MySwiftUISourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "Hello World"
+    @State private var title: String = "Hello Hakob"
     
     var body: some View {
         VStack {
