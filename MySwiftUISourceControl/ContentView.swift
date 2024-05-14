ed2c9028db69d0dbef0cfca2cc3828e2860d77a7
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-14 15:26:46 +0400

    [Clean] Update Documentation

--- MySwiftUISourceControl/ContentView.swift
+++ MySwiftUISourceControl/ContentView.swift
@@ -8,12 +8,29 @@
 // COMMIT MESSAGES
 /*
  COMMIT MESSAGES
- Clone      -    Copying the repo loaclly
- Commit     -    Save ("checkpoint") on our current branch
- Stage      -    Preparechanges for a commit
- Stash      -    Save changes for later
- Push       -    Send local commits to remote repo
- Pull       -    Fetch remote commits to local repo
+ Clone                  -    Copying the repo loaclly
+ Commit                 -    Save ("checkpoint") changes on our current branch
+ Stage                  -    Prepare changes for a commit
+ Stash                  -    Save changes for later
+ Push                   -    Send local commits to remote repo
+ Pull                   -    Fetch remote commits to local repo
+ Merge                  -    Joining two different branch
+ Rebase                 -    Moving one branch on top of another branch
+ Cherry picking         -    Duplicatin (copying) one commit from one branch to another
+ 
+ --------------------------------------------------------------------
+ 
+ Clone                  -    Копирование репозитория локально
+ Commit                 -    Сохранение изменений ("контрольная точка") в нашей текущей ветке
+ Stage                  -    Подготовка изменений к фиксации
+ Stash                  -    Сохранение изменений для последующего использования
+ Push                   -    Отправка локальных коммитов в удаленное хранилище
+ Pull                   -    Извлечение удаленных коммитов в локальное хранилище
+ Слияние                -    соединение двух разных ветвей
+ Перебазирование        -    Перемещение одной ветви поверх другой ветви
+ Сбор данных            -    Дублирование (копирование) одного коммита из одной ветви в другую.
+ 
+ --------------------------------------------------------------------
  
  NEW FEATURE:
  [Feature] Description of the feature
