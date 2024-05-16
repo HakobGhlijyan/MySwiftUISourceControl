f36db6b225fb6c92f9ea946e454ebcf92729498a
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-16 13:01:26 +0400

    [Feature] Add Settings View

---
+++ MySwiftUISourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MySwiftUISourceControl
+//
+//  Created by Hakob Ghlijyan on 16.05.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
