74a5c30c4cd112ec6761eba6706bd500420d522d
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-19 17:44:35 +0400

    [Feature] Add Profile view

---
+++ MySwiftUISourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MySwiftUISourceControl
+//
+//  Created by Hakob Ghlijyan on 19.05.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hakob")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
